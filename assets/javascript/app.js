27bb304f7031a8545127c49c83b754ae955233d6
mapachurro <oliver.renwick@gmail.com>
2018-11-21 08:45:06 -0500

    a buncha stuff working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,48 +21,82 @@ $(document).ready(function () {
 
 
 //The object containing the questions that we will be deploying:
-    var Questions = {   
-            Whatis: {
-                Question: "What is anarcho-syndicalism?",
+    var Questions = { 
+        questionArray: [
+             {  Question: "What is anarcho-syndicalism?",
                 Answer1: "A sort of commune where you elect a sort of executive officer of the week",
                 Answer2: "A kind of anarchism that's set up in cells part of a larger whole",
                 Answer3: "The belief that anarchical organizing of the working class will help lead to broader societal revolution",
             },
-            Author : {
-                Question: "Which English-speaking author documented up-close the realities of life in pre-dictatorship Catalunya?",
+            {   Question: "Which English-speaking author documented up-close the realities of life in pre-dictatorship Catalunya?",
                 Answer1: "Orwell",
                 Answer2: "Hemingway",
                 Answer3: "Fitzgerald",
             },
-            Organization : {
-                Question: "Which method does anarcho-syndicalism most clearly embrace?",
+            {   Question: "Which method does anarcho-syndicalism most clearly embrace?",
                 Answer1: "Representative democratic voting",
                 Answer2: "The overthrow en masse of the bourgeoise",
                 Answer3: "Direct action by the workers",           
             },
-            Colors : {
-                Question: "What main color do anarchists use in their symbols?",
+            {   Question: "What main color do anarchists use in their symbols?",
                 Answer1: "Black",
                 Answer2: "Red",
                 Answer3: "Yellow",
             },
-            Symbols : {
-                Question: "What animal is associated with the Industrial Workers of the World?",
+            {   Question: "What animal is associated with the Industrial Workers of the World?",
                 Answer1: "A hawk",
                 Answer2: "A cat",
                 Answer3: "A wolverine",
             },
-            
-    }
+        ]}
+
+//Additional variables that will be needed:
+    var correct = "";
+    var incorrect = "";
+    var unaswered = "";
+    var questionIndex = [0];
+    var time = 21;      
 
 
+//Gameplay itself. First, the initial click:
     $("#playButton").on("click", function() {
         $("#instructions").css({"visibility": "hidden"});
         $("#quizCard").css({"visibility": "visible"})
-        var time = 20;
         run();
         questionLoop();
+    })
 
+//Then, subsequent questions:
+    $("#answer1, #answer2, #answer3").on("click", function() {
+       if (questionIndex < 4) { 
+        //First, clear out the html:
+        $("#question").empty();
+        $("#answer1").empty();
+        $("#answer2").empty();
+        $("#answer3").empty();
+        
+        //Stop the timer
+        stop();
+        //Then, increase the question index to move on to the next question:
+        console.log("Mr. Hammond, the radio buttons are working");
+        questionIndex++;
+        console.log("question index " + questionIndex);
+        //And load the next question:
+        questionLoop();
+        //Reset time to 20
+        time = 21;
+        //Start the timer: 
+        run();
+    }
+        else{
+            alert("Congratulations! You finished the quiz.")
+            stop();
+        }
+
+    })
+
+         //  When the stop button gets clicked, run the stop function.
+$("#nextButton").on("click", stop());    
 
 //Below are functions that will be running during gameplay, such as the functions regarding
 //the timeout, and then functions regarding the questions and answers.
@@ -88,42 +122,47 @@ $(document).ready(function () {
           alert("Time Up!");
         }
       }
-    //  When the stop button gets clicked, run the stop function.
-    $("#nextButton").on("click", stop);
 
-    function stop() {
-        //  Clears our intervalId
-        //  We just pass the name of the interval
-        //  to the clearInterval function.
-        clearInterval(intervalId);
-        console.log("done");
-        $("#time-left").append("<h2>Time's Up!</h2>");
-        console.log("time is up");
-      }
+//The 'stop' function needs to be available globally, so here it is:
+function stop(){
+    //  Clears our intervalId
+    //  We just pass the name of the interval to the clearInterval function.
+    clearInterval(intervalId);
+    console.log("done");
+    console.log("time is up");
+}
 
-    //Function for looping through the questions:
+
+      //Priorities, in this order: Make it be one question at a time. DONE
+      //Make it move on to the next question on the click. DONE
+      //Make the timer work on the click. DONE
+      //Store variables. 
+      //Compare scores at the end.
+
+      //increment the index of the question you're on with each question you answer. DONE
+      // Don't do a loop through. DONE
+      //create an array of correct answers, and collect the answers as the game progresses; compare the two
+      //arrays at the end using a for loop.
+
+    // Function for looping through the questions:
       function questionLoop(){
-          }
-          for (let index = 0; index < Questions.length; index++) {
-                const element = Questions[index];
-                const question = Questions[index].Question;
-                const answer1 = Questions[index].Answer1;
-                const answer2 = Questions[index].Answer2;
-                const answer3 = Questions[index].Answer3;
+        //Set up the variables, within the limited scope of this function, for the present question:
+          console.log ("at beginning of loop, question index is " + questionIndex);
+        const question = Questions.questionArray[questionIndex].Question;
+        const answer1 = Questions.questionArray[questionIndex].Answer1;
+        const answer2 = Questions.questionArray[questionIndex].Answer2;
+        const answer3 = Questions.questionArray[questionIndex].Answer3;
 
-                $("#question").html(question);
-                console.log(question);
-                $("#answer1").html(answer1);
-                $("#answer2").html(answer2);
-                $("#answer3").html(answer3);
+        //Then fill the html with those variables:
+        var thisQuestion = $('<h2>' + question + '</h2>');
+        thisQuestion.appendTo("#question");
+        var radioBtn1 = $('<input type="radio" name="rbtnCount">' + answer1 + '</>');
+        radioBtn1.appendTo('#answer1');
+        console.log(answer1);
+        var radioBtn2 = $('<input type="radio" name="rbtnCount">' + answer2 + '</>');
+        radioBtn2.appendTo('#answer2');
+        var radioBtn3 = $('<input type="radio" name="rbtnCount">' + answer3 + '</>');
+        radioBtn3.appendTo('#answer3');
+    }
       
-          }
-
-
-      }
-)
-
-
-
-
-})
+    })
